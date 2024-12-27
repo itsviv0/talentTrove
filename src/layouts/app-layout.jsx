df707b47c34427906df5ad809b146bd29214313e
itsviv0 <vivekganeshs12@gmail.com>
2024-12-27 14:38:58 +0530

    Added theme provider and enhance app layout with background styling.

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 const AppLayout = () => {
   return (
     <div>
-      AppLayout
+      <div className="journal-bg"></div>
       <Outlet />
     </div>
   );
