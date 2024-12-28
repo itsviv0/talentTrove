97d7325f500c63453c5fca9f18e0342df95918e2
itsviv0 <vivekganeshs12@gmail.com>
2024-12-28 20:40:05 +0530

    Refactored app-layout.tsx to have the header component and fixed the width issue.

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -1,3 +1,4 @@
+import Header from "@/components/molecules/header";
 import React from "react";
 import { Outlet } from "react-router-dom";
 
@@ -5,7 +6,19 @@ const AppLayout = () => {
   return (
     <div>
       <div className="journal-bg"></div>
-      <Outlet />
+      <div className="focus-glow"></div>
+      <main
+        className="min-h-screen container"
+        style={{
+          position: "relative",
+          zIndex: 1,
+          minWidth: "100vw",
+          overflowX: "hidden",
+        }}
+      >
+        <Header />
+        <Outlet />
+      </main>
     </div>
   );
 };
